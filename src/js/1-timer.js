a34ecfd6f75d1d4a8342390382708c30900700e5
bndnt <67003594+bndnt@users.noreply.github.com>
2024-06-18 12:54:12 -0400

    тзь

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,7 +1,7 @@
-// import flatpickr from 'flatpickr';
-// import 'flatpickr/dist/flatpickr.min.css';
-// import iziToast from 'izitoast';
-// import 'izitoast/dist/css/iziToast.min.css';
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 const datetimePicker = document.querySelector('#datetime-picker');
 const startButton = document.querySelector('[data-start]');
